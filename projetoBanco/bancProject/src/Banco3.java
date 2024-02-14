2ddd6928a0039c95a868c519c93fd93ec7cc069d
mateus-neres <mateusneres03@gmail.com>
2024-02-14 19:22:07 -0300

    .

--- projetoBanco/bancProject/src/Banco3.java
+++ projetoBanco/bancProject/src/Banco3.java
@@ -1,2 +1,32 @@
+//Escreva um programa chamado CaixaChatBot.
+
+import banco.entidades.ContaSimples;
 public class Banco3 {
+    public static void main(String[] args) {
+        //1. para criar uma nova conta (que deve ser salva na agência)
+        ContaSimples umaConta = new ContaSimples("Mateus", "700293444-92");
+
+        //2. para localizar uma conta já existente através de seu número
+        
+
+        //Com a conta identificada o menu de opções do bot deve mudar o menu para as seguintes opções:
+        //1. Depositar
+        //2. Sacar
+        //3. Extrato
+        //4. Transferir
+        //5. Saldo
+        //6. Sair
+
+
+        /*Realize o desejo do usuário e em seguida, se a opção não é a de sair, apresente o menu novamente.
+          Para cada opção é preciso solicitar do usuário os dados necessários para a operação.
+          Se o usuário quer sacar, pergunte quanto e saque... Se quer transferir pergunte para que conta (número) e o valor...*/
+
+        /*Fique neste loop até que o usuário deseje sair do programa.
+          Antes de terminar o programa imprima o saldo final da conta e feche a agência para salvar a conta manipulada.*/
+
+        /*Anexe aqui o arquivo CaixaEletronico.java que você desenvolver.
+          Lembre de escrever um código fácil de entender e que segue as regras de estilo de programação Java.*/
+
+    }
 }
