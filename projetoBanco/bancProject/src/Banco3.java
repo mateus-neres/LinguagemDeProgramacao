ffe93781e5639fa3be8d369eb7e7f366b729ce3c
mateus-neres <mateusneres03@gmail.com>
2024-02-18 20:33:20 -0300

    Update Banco3.java

--- projetoBanco/bancProject/src/Banco3.java
+++ projetoBanco/bancProject/src/Banco3.java
@@ -1,5 +1,25 @@
 public class Banco3 {
     public static void main(String[] args) {
+// cria dois objetos Pessoa com nome e CPF:
 
+        // pessoa1 é Joao, com CPF 30914060506
+
+        // pessoa1 é Ana, com CPF 12345678901
+
+// cria as duas contas,
+
+        // uma cujo titular é João e
+
+        // outra cujo titular é Ana
+
+// solicite do usuário o valor a depositar na conta de Joao
+
+// deposite este valor na conta de João
+
+// solicite do usuário o valor a transferir de João para Ana
+
+// transfira este valor da conta de João para a conta de Ana
+
+// Fecha a agencia e guarde toda a informação em arquivo
     }
 }
