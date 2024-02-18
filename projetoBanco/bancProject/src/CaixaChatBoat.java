04b786bd55630ec95cbf45691c2adcd2e58cb023
mateus-neres <mateusneres03@gmail.com>
2024-02-18 20:02:33 -0300

    Update CaixaChatBoat.java

--- projetoBanco/bancProject/src/CaixaChatBoat.java
+++ projetoBanco/bancProject/src/CaixaChatBoat.java
@@ -1,123 +1,155 @@
-//Escreva um programa chamado CaixaChatBot.
-
-//1. para criar uma nova conta (que deve ser salva na agência)
-//Com a conta identificada o menu de opções do bot deve mudar o menu para as seguintes opções:
-//1. Depositar
-//2. Sacar
-//3. Extrato
-//4. Transferir
-//5. Saldo
-//6. Sair
-
-/*Realize o desejo do usuário e em seguida, se a opção não é a de sair, apresente o menu novamente.
-          Para cada opção é preciso solicitar do usuário os dados necessários para a operação.
-          Se o usuário quer sacar, pergunte quanto e saque... Se quer transferir pergunte para que conta (número) e o valor...*/
-
-        /*Fique neste loop até que o usuário deseje sair do programa.
-          Antes de terminar o programa imprima o saldo final da conta e feche a agência para salvar a conta manipulada.*/
-
-        /*Anexe aqui o arquivo CaixaEletronico.java que você desenvolver.
-          Lembre de escrever um código fácil de entender e que segue as regras de estilo de programação Java.*/
-
 import banco.entidades.Agencia;
 import banco.entidades.Conta;
 import banco.entidades.ContaSimples;
+import banco.entidades.Extrato;
+import banco.entidades.util.Data;
 
 import java.util.Scanner;
 
 public class CaixaChatBoat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        boolean sair = false;
+        while (!sair) {
             exibirMenuInicial();
             int escolha = scanner.nextInt();
             scanner.nextLine();
             switch (escolha) {
                 case 1:
-                    criarNovaConta();
+                    criarNovaConta(scanner);
                     break;
                 case 2:
-                    encontrarConta();
+                    encontrarConta(scanner);
+                    break;
+                case 3:
+                    sair = true;
                     break;
                 default:
-                    System.out.println("Digite uma opção valida");
+                    System.out.println("Digite uma opção válida");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void exibirMenuConta(Conta conta, Scanner scanner) {
+        while (true) {
+            exibirMenuContas();
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+            switch (escolha) {
+                case 1:
+                    System.out.println("Qual valor você quer depositar?");
+                    double valorDeposito = scanner.nextDouble();
+                    conta.depositar(valorDeposito);
+                    System.out.println("Valor depositado.");
+                    break;
+                case 2:
+                    System.out.println("Qual valor você quer sacar?");
+                    double valorSaque = scanner.nextDouble();
+                    conta.sacar(valorSaque);
+                    System.out.println("Valor Sacado.");
+                    break;
+                case 3:
+                    operacaoExtract(conta, scanner);
+                    break;
+                case 4:
+                    operacaoTransfer(conta, scanner);
+                    break;
+                case 5:
+                    System.out.println("Saldo atual: " + conta.getSaldo());
+                    break;
+                case 6:
+                    Agencia.fecharCaixa();
+                    return;
+                default:
+                    System.out.println("Digite uma opção válida");
             }
         }
     }
 
-    private static void encontrarConta() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o número da conta que deseja encontrar: ");
-        long numeroConta = scanner.nextLong();
-        Conta conta = Agencia.localizarConta(numeroConta);
-        System.out.println(conta);
-        exibirMenuConta(conta);
+    public static String lerEntradaDoUsuario(Scanner scanner) {
+        return scanner.nextLine();
     }
 
-    public static String lerEntradaDoUsuario(){
-        Scanner scanner = new Scanner(System.in);
-        String entrada = scanner.nextLine();
-        return entrada;
-    }
-    public static void exibirMenuInicial(){
-        System.out.printf("%n=========Escolha uma das opções=============%n");
-        System.out.printf("( 1 ).Para criar uma conta.%n");
-        System.out.printf("( 2 ).Para buscar uma conta pelo número.%n");
-        System.out.printf("============================================%n");
+    public static void exibirMenuInicial() {
+        System.out.println("========= Escolha uma das opções =========");
+        System.out.println("(1). Para criar uma conta.");
+        System.out.println("(2). Para buscar uma conta pelo número.");
+        System.out.println("(3). Para Sair.");
+        System.out.println("===========================================");
         System.out.print("Escolha uma opção: ");
-
     }
 
-    public static void exibirMenuConta(Conta conta){
-            Scanner scanner = new Scanner(System.in);
-            int selecao = scanner.nextInt();
-            System.out.printf("%n=========Menu da conta=============%n");
-            System.out.println("( 1 ). Depositar");
-            System.out.println("( 2 ). Sacar");
-            System.out.println("( 3 ). Extrato");
-            System.out.println("( 4 ). Transferir");
-            System.out.println("( 5 ). Saldo");
-            System.out.println("( 6 ). Sair da conta");
-            System.out.printf("============================================%n");
-            System.out.print("Escolha uma opção: ");
-            /*switch (selecao) {
-                case 1:
-                    System.out.println("Digite o valor a ser depositado: ");
-                    double valor = scanner.nextDouble();
-                    conta.depositar(valor);
-                    System.out.println(conta.getSaldo());
-                    break;
-                case 2:
-                    Conta.sacar();
-                    break;
-                case 3:
-                    Conta.criarExtrato();
-                    break;
-                case 4:
-                    Conta.transferir();
-                    break;
-                case 5:
-                    Conta.saldo();
-                    break;
-                case 6:
-                    exibirMenuInicial();
-
-
-                default:
-                    System.out.println("Digite uma opção valida");
-            }*/
+    public static void exibirMenuContas() {
+        System.out.println("========= Menu da conta =========");
+        System.out.println("1. Depositar");
+        System.out.println("2. Sacar");
+        System.out.println("3. Extrato");
+        System.out.println("4. Transferir");
+        System.out.println("5. Saldo");
+        System.out.println("6. Sair da conta");
+        System.out.println("================================");
+        System.out.print("Escolha uma opção: ");
     }
 
-
-    public static void criarNovaConta(){
+    public static void criarNovaConta(Scanner scanner) {
         String nome, cpf;
         System.out.print("Digite o nome completo: ");
-        nome = lerEntradaDoUsuario();
+        nome = lerEntradaDoUsuario(scanner);
         System.out.print("Digite o CPF: ");
-        cpf = lerEntradaDoUsuario();
+        cpf = lerEntradaDoUsuario(scanner);
         System.out.println();
         ContaSimples umaConta = new ContaSimples(nome, cpf);
         Agencia.addConta(umaConta);
-        System.out.println("Conta adicionada! O numero da sua conta é: " + umaConta.getNúmero());
+
+        System.out.println("Conta adicionada! Seu número de conta é " + umaConta.getNúmero());
     }
+
+    private static void encontrarConta(Scanner scanner) {
+        System.out.println("Digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        Conta conta = Agencia.localizarConta(numeroConta);
+        if (conta != null) {
+            exibirMenuConta(conta, scanner);
+        } else {
+            System.out.println("Conta não encontrada.");
+        }
+    }
+
+    public static void operacaoTransfer(Conta conta, Scanner scanner) {
+        System.out.println("Qual valor você quer transferir?");
+        double valorTransferencia = scanner.nextDouble();
+        System.out.println("Digite o número da conta de destino:");
+        int numeroDestino = scanner.nextInt();
+        Conta contaDestino = Agencia.localizarConta(numeroDestino);
+        if (contaDestino != null) {
+            conta.transferir(contaDestino, valorTransferencia);
+            System.out.println("Transferência realizada com sucesso.");
+        } else {
+            System.out.println("Conta de destino não encontrada.");
+        }
+    }
+
+    public static void operacaoExtract(Conta conta, Scanner scanner) {
+        System.out.println("Extrato da conta:");
+        System.out.println("Informe o dia de abertura do extrato:");
+        int diaI = scanner.nextInt();
+        System.out.println("Informe o mês de abertura do extrato:");
+        int mesI = scanner.nextInt();
+        System.out.println("Informe o ano de abertura do extrato:");
+        int anoI = scanner.nextInt();
+        System.out.println("Informe o dia de fechamento do extrato:");
+        int diaF = scanner.nextInt();
+        System.out.println("Informe o mês de fechamento do extrato:");
+        int mesF = scanner.nextInt();
+        System.out.println("Informe o ano de fechamento do extrato:");
+        int anoF = scanner.nextInt();
+        Data dataInit = new Data(diaI, mesI, anoI);
+        Data dataFim = new Data(diaF, mesF, anoF);
+        Extrato extrato = conta.criarExtrato(dataInit, dataFim);
+        System.out.println(extrato.formatar());
+
+    }
+
 }
