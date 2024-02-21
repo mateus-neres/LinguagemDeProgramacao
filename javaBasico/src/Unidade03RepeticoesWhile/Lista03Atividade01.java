3598e29738a9412d3b32092aa44b851592e39adf
mateus-neres <mateusneres03@gmail.com>
2024-02-20 21:52:06 -0300

    .

--- javaBasico/src/Unidade03RepeticoesWhile/Lista03Atividade01.java
+++ javaBasico/src/Unidade03RepeticoesWhile/Lista03Atividade01.java
@@ -1,5 +1,22 @@
 package Unidade03RepeticoesWhile;
 
-public class Lista03Atividade01 {
+import java.util.Scanner;
 
+public class Lista03Atividade01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String controle = "";
+        String coletarLetras;
+        int contador = 0;
+        while (!controle.toUpperCase().equals("X")) {
+            System.out.println("Digite uma letra: ");
+            coletarLetras = scanner.nextLine();
+
+            if (coletarLetras.toUpperCase().equals("W") || coletarLetras.toUpperCase().equals("Y") || coletarLetras.toUpperCase().equals("K")) {
+                contador ++;
+            }
+            controle = coletarLetras.toUpperCase();
+        }
+        System.out.println(contador);
+    }
 }
--- javaBasico/src/Unidade04RepeticoesFor/Lista02Atividade02.java
+++ javaBasico/src/Unidade04RepeticoesFor/Lista02Atividade02.java
@@ -1,4 +1,34 @@
 package Unidade04RepeticoesFor;
 
+import java.util.Scanner;
+
 public class Lista02Atividade02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int contaVotoMarcilio = 0;
+        int contaVotoAurelio = 0;
+        int contaVotoBranco = 0;
+        String voto;
+        for (int i = 0 ; i < 4 ; i++) {
+            System.out.println("Digite em quem deseja votar: ");
+            voto = scanner.nextLine();
+            if (voto.toUpperCase().equals("MARCILIO")) {
+                contaVotoMarcilio ++;
+            } else if (voto.toUpperCase().equals("AURELIO")) {
+                contaVotoAurelio ++;
+            } else {
+                contaVotoBranco ++;
+            }
+        }
+        if (contaVotoAurelio > contaVotoMarcilio & contaVotoAurelio > contaVotoBranco) {
+            System.out.println("Aurélio");
+        } else if (contaVotoMarcilio > contaVotoAurelio & contaVotoMarcilio > contaVotoBranco) {
+            System.out.println("Márcílio");
+        } else if (contaVotoBranco >= contaVotoMarcilio || contaVotoBranco >= contaVotoAurelio) {
+            System.out.println("Nova votação");
+        } else {
+            System.out.println("Nova votação");
+        }
+    }
 }
+
